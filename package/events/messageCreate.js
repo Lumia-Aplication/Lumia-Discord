554acfe1ad544fad1f1bd1962c15a6b1ae084169
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-01 18:02:22 -0300

    create user in database

--- package/events/messageCreate.js
+++ package/events/messageCreate.js
@@ -1,17 +1,22 @@
 const { database } = require('@lumia/client/database.js');
+const User = require('@lumia/schemas/user.js');
 
 const prefix = '.';
 
-module.exports = (client, message) => {
+module.exports = async (client, message) => {
   if(!message.content.startsWith(prefix) || message.author.bot) return;
-      
+  
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
-        
+  
   const command = client.commands.get(commandName);
-      
+  
   if (!command) return;
-        
+  const user = await User.findOne({ userId: message.author.id });
+  if(!user) {
+    await User.create({ userId: message.author.id });
+    return message.reply({ content: 'Você foi adicionado ao banco de dados, execute novamente', ephemeral: true });
+  }
   try {
     command.execute(client, message, args, database);
   } catch (error) {
