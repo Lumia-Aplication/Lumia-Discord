955689a4820870f920fe87fda60cacc84e724f75
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-01 18:01:51 -0300

    change ping command

--- package/commands/bot/ping.js
+++ package/commands/bot/ping.js
@@ -1,12 +1,9 @@
-const constructToEmbed = require('../../_partials/construcEmbed');
-
 module.exports = {
   name: 'ping',
   description: 'Descrição do comando',
   execute(client, message) {
     const { t } = client;
 
-    const embed = constructToEmbed(t('ping', { ping: client.ws.ping }));
-    return message.reply({ embeds: [embed]});
+    return message.reply({ content: t('ping', { ping: client.ws.ping }) });
   },
 };
