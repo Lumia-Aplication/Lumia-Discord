b47544b4daca2ac40cafcc2d8d28eec7db23517c
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-02 00:19:11 -0300

    Update locales

--- src/locales/br.js
+++ src/locales/br.js
@@ -1,3 +1,10 @@
 module.exports = {
+  langEN: 'Inglês(en)',
+  langBR: 'Português(br)',
   ping: '🏓 Meu ping: {{ping}}!',
+  setlang: {
+    title: 'Olá {{username}}, selecione uma linguagem para você',
+    description: 'A sua linguagem atual é: **{{userLang}}**\n\nLinguagem padrão: **{{langDefault}}**',
+    successChange: 'Linguagem alterada para {{setedLang}}!'
+  }
 };
--- src/locales/en.js
+++ src/locales/en.js
@@ -1,3 +1,10 @@
 module.exports = {
+  langEN: 'English (en)',
+  langBR: 'Portuguese (br)',
   ping: '🏓 My ping: {{ping}}!',
+  setlang: {
+    title: 'Hello {{username}}, select a language for yourself',
+    description: 'Your current language is: **{{userLang}}**\n\nDefault language: **{{langDefault}}**',
+    successChange: 'Language changed to {{setedLang}}!'
+  }
 };
