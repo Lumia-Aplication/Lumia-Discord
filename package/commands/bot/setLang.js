0af829a8b136e58f28287672626bce46e74fb79b
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-02 00:19:47 -0300

    fix setlang user

--- package/commands/bot/setLang.js
+++ package/commands/bot/setLang.js
@@ -1,4 +1,5 @@
 const { constructToActionRow, constructToButton, constructToEmbed } = require('../../_partials');
+const { ButtonStyle } = require('discord.js');
 const setLang = require('../../_partials/guild/setLang');
 const User = require('@lumia/schemas/user.js');
 
@@ -6,42 +7,57 @@ module.exports = {
   name: 'setlang',
   description: 'Setar uma linguagem ao usuario',
   async execute(client, message) {
+    const { t } = client;
+
+    const user = await User.findOne({ userId: message.author.id });
+
+    const contentEmbed = {
+      title: t('setlang.title', { lng: user.lang, username: message.author.username }),
+      description: t('setlang.description', { lng: user.lang, langDefault: t('langBR', { lng: user.lang }), userLang: user.lang.replace('en', t('langEN', { lng: user.lang })).replace('br', t('langBR', { lng: user.lang })) }),
+      user: message.author
+    };
+
     const buttons = [
       {
-        label: 'Portugues(BR)',
-        customId: 'br'
+        label: t('langBR', { lng: user.lang }),
+        customId: 'br',
+        icon: '<:Brazil:1124893878554734706>',
+        color: ButtonStyle.Primary
       },
       {
-        label: 'Inglês(en)',
-        customId: 'en'
+        label: t('langEN', { lng: user.lang }),
+        customId: 'en',
+        icon: '<:flag_us48:1124894096570466344>',
+        color: ButtonStyle.Danger
       }
     ];
-    const user = await User.findOne({ userId: message.author.id });
     const button = constructToButton(buttons);
     const row = constructToActionRow(button);
-    const embed = constructToEmbed(`Selecione sua linguagem, atual: ${user.lang}`);
+
+    const embed = constructToEmbed(contentEmbed.user, contentEmbed.title, contentEmbed.description);
   
     const messageChannel = await message.reply({ embeds: [embed], components: [row] });
     
     const filter = (interaction) => interaction.isButton() && interaction.user.id === message.author.id;
 
-    const collector = messageChannel.createMessageComponentCollector({ filter, time: 15000 });
+    const collector = messageChannel.createMessageComponentCollector({ filter });
 
     collector.on('collect', async (interaction) => {
       if (interaction.customId == 'br') {
+
         await setLang(interaction.user.id, 'br');
         await interaction.deferUpdate();
-        return message.channel.send({ content: 'Linguagem alterada para Portugues(BR)', Ephemeral: true });
+        await messageChannel.delete();
+
+        return message.channel.send({ content: t('setlang.successChange', { lng: user.lang, setedLang: t('langBR') })});
       } else if (interaction.customId == 'en') {
+
         await setLang(interaction.user.id, 'en');
         await interaction.deferUpdate();
-        return message.channel.send({ content: 'Linguagem alterada para Inglês(EN)', Ephemeral: true });
-      }
-    });
+        await messageChannel.delete();
 
-    collector.on('end', () => {
-      messageChannel.delete();
-      return message.channel.send({ content: 'Tempo para selecionar acabou', Ephemeral: true });
+        return message.channel.send({ content: t('setlang.successChange', { lng: user.lang, setedLang: t('langEN') })});
+      }
     });
   }
 };
