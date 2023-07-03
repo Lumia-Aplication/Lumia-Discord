23be07673bdde57255311844f99caa75d2a44aa0
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-03 18:28:25 -0300

    :sparkles: Add new feature to embed, footer and fields

--- package/_partials/construcEmbed.js
+++ package/_partials/construcEmbed.js
@@ -1,12 +1,22 @@
 const { EmbedBuilder } = require('discord.js');
 
-function constructToEmbed(user, title = '', description = '', color = '#FFB347') {
-  return new EmbedBuilder()
+function constructToEmbed(user, title = '', description = '', footer = '', color = '#FFB347', ...fields) {
+  const embed = new EmbedBuilder()
     .setThumbnail(user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
     .setTitle(title)
     .setDescription(description)
-    .setColor(color)
+    .setColor(color.replace('skip', '#FFB347'))
     .setTimestamp();
+  if(footer) {
+    embed.setFooter({ text: footer });
+  }
+  if (fields.length > 0) {
+    for (const param of fields) {
+      embed.addFields({ name: param.name, value: param.value, inline: param.inline });
+    }
+  }
+
+  return embed;
 }
 
 module.exports = constructToEmbed;
