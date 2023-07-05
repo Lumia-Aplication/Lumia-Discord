57830c8c64e4c0717e6c966bafcfb50bca4bcda3
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-05 17:02:24 -0300

    :fire: Remove file

--- package/slashCommands/teste/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'pong',
-  description: 'Descrição do comando',
-  async execute(client, message) {
-    console.log(client,message);
-    return client.reply('ping');
-  },
-};
