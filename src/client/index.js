9ce440ca8b8c122adb4f159e2cf33a71bf2aa812
Gabriel Mousquer <gabrielmousquer1227@gmail.com>
2023-07-20 13:14:35 -0300

    :art: New require
    dadw

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 require('../modules/database');
 require('../modules/alias');
+require('./server');
 const configureAtributes = require('./atributes');
 const loadHandlers = require('../_partials');
 
