1e3cbaa3b96beea5f008b43967fafa4df95c13a9
Tashimi2040 <ivisnayara@gmail.com>
2023-07-20 14:03:30 -0300

    :art: Added bot-info
    Bot-info command added

--- src/schemas/guild.js
+++ src/schemas/guild.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const guildSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
-  prefix: { type: String, default: '.' }, 
+  prefix: { type: String, default: '.' },
+  usages: { type: Number, default: 0},
+  lastReset: {type: Date, default: Date.now() }
 });
 
 const Guild = mongoose.model('Guild', guildSchema);
